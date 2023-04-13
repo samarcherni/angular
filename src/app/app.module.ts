b08537f96159fa24d26bbac56a5d355335870b25
samarcherni <samar.cherni@esprit.tn>
2023-04-13 10:20:53 +0100

    atelier 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
+import { FormsModule } from '@angular/forms';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffresEmploiComponent,
+    ArticlesComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
